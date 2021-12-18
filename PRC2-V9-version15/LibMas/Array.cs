d0a13c07a9cfc7cbac424a3e22b8d1b0e47cc7cb
Mishin Dmitiriy <93830209+Harakiri-22@users.noreply.github.com>
2021-12-18 11:21:29 +0300

    Add files via upload

---
+++ PRC2-V9-version15/LibMas/Array.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.IO;
+
+namespace LibMas
+{
+    public class Array
+    {
+        private readonly Random _random = new Random();
+        private int[] _array;
+        public int Length => _array.Length;
+
+        public Array(int length)
+        {
+            _array = new int[length];
+        }
+
+        public void Fill(int min = -20, int max = 20)
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                _array[i] = _random.Next(min, max);
+            }
+        }
+
+        public void Import(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                _array = new int[int.Parse(reader.ReadLine())];
+
+                for (int i = 0; i < Length; i++)
+                {
+                    _array[i] = int.Parse(reader.ReadLine());
+                }
+            }
+        }
+        public void Export(string path)
+        {
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine(Length);
+
+                for (int i = 0; i < Length; i++)
+                {
+                    writer.WriteLine(_array[i]);
+                }
+            }
+        }
+
+        public DataTable ToTableData()
+        {
+            var result = new DataTable();
+            for (int i = 0; i < _array.Length; i++)
+            {
+                result.Columns.Add("Col" + (i + 1));
+            }
+            var row = result.NewRow();
+            for (int i = 0; i < Length; i++)
+            {
+                row[i] = _array[i];
+            }
+            result.Rows.Add(row);
+            return result;
+        }
+
+        public int this[int index]
+        {
+            get => _array[index];
+            set => _array[index] = value;
+        }
+
+        public void Clear()
+        {
+            Fill(0, 0);
+        }
+    }
+}
---
+++ PRC2-V9-version15/Lib_9/Multiply.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibMas;
+
+namespace Lib_9
+{
+    public static class Multiply
+    {
+        public static string MultiplyingAnArray(this LibMas.Array array)
+        {
+            int product = 1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != 0)
+                {
+                    product *= array[i];
+                }
+            }
+            return product.ToString();
+        }
+    }
+}
